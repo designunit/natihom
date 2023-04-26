fc1efb8e3a15c4b84eaffe093b902b2c99867915
manhir <53478716+manhir@users.noreply.github.com>
2023-04-26 18:07:16 +0300

    move /2022 to component and upd on mobile

---
+++ src/components/Page2022/index.tsx
@@ -0,0 +1,147 @@
+import { Button } from 'src/components/Button'
+import { Section } from 'src/components/Section'
+import { Flex } from 'src/components/Flex'
+import Image from 'next/image'
+import { Parallax } from 'react-scroll-parallax'
+import aboutText from '../../../public/aboutText.png'
+import glitch from '../../../public/glitch.gif'
+import cog from '../../../public/cog.png'
+import textImg from '../../../public/2022Text.png'
+import { useMobile } from 'src/hooks/useMobile'
+import s from './index.module.css'
+
+export const Page2022 = () => {
+    const isMobile = useMobile()
+    return (
+        <>
+            {!isMobile && (
+                <div style={{
+                    height: '10rem'
+                }} />
+            )}
+
+            <Section
+                className={s.video}
+            >
+                <Flex>
+                    <Flex col>
+                        <b>место: оз. Тихое</b>
+                        <b>Даты: 16-18 Августа 2022</b>
+                        <span> <b>Музыканты:</b> BARKAL, DRKBLCK, LE BEAU SERGE, Dj Countryman,Low Priority, Dj Countryman, Derun, Ply Tone, MR. GUYVER</span>
+                        <span><b>Спикеры:</b> BARKAL, DRKBLCK, LE BEAU SERGE, Dj Countryman,Low Priority, Dj Countryman, Derun, Ply Tone, MR. GUYVER</span>
+                        <span>Мы разрабатываем средовые проекты разных масштабов: от урны до мастерплана. Этим летом мы придумали лесную резиденцию архитекторов, дизайнеров и всех причастных. Идея резиденции возникла из всеобщего запроса на поиск смыслов, расширение экосистемы профессионального сообщества в непривычном формате и средовом окружении леса и палаточного лагеря. Мы запустили проектную лабораторию и со студентами готовим инфраструктуру резиденции на берегу озера. Лекции, воркшопы и дискуссии об архитектуре и городе, культуре и искусстве, настоящем и будущем, музыка, танцы, арт, фудкорт, чиллзона закат и рассвет.</span>
+                    </Flex>
+                    <iframe
+                        src="https://www.youtube.com/embed/7E0ot9iJm_k"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                        style={{
+                            flex: '1 0 50%',
+                            border: 'none',
+                            aspectRatio: '16 / 9',
+                        }}
+                    />
+                </Flex>
+            </Section>
+
+            <div style={{
+                height: 30,
+            }} />
+
+            {!isMobile ? (
+                <Section bg>
+                    <Parallax
+                        scaleX={[1, 1.5]}
+                        translateX={[0, 25]}
+                        shouldAlwaysCompleteAnimation
+                        // disabled={isMobile}
+                        style={{
+                            // position: 'absolute',
+                            // top: 400,
+                            // right: -20,
+                        }}
+                    >
+                        <Image
+                            src={glitch}
+                            alt='гличуха'
+                        />
+                    </Parallax>
+                    <Parallax
+                        rotate={[0, 360]}
+                        shouldAlwaysCompleteAnimation
+                        // disabled={isMobile}
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            right: -20,
+                        }}
+                    >
+                        <Image
+                            src={cog}
+                            alt='хромированая шестерня'
+                        />
+                    </Parallax>
+                </Section>
+            ) : (
+                <Section bg
+                    style={{
+                        zIndex: 3,
+                    }}
+                >
+                    <Parallax
+                        rotate={[0, 360]}
+                        shouldAlwaysCompleteAnimation
+                        style={{
+                            position: 'absolute',
+                            top: -50,
+                            right: 10,
+                        }}
+                    >
+                        <Image
+                            src={cog}
+                            alt='хромированая шестерня'
+                            width={100}
+                            height={100}
+                        />
+                    </Parallax>
+                </Section>
+            )}
+
+            {!isMobile && (
+                <Section>
+                    <Flex>
+                        <div style={{
+                            flex: '1 0 33%'
+                        }} />
+                        <Image
+                            src={aboutText}
+                            alt='текст про фестиваль в целом'
+                        />
+                    </Flex>
+                </Section>
+            )}
+
+            <Section
+                style={{
+                    top: isMobile ? 0 : '-13rem',
+                }}
+                className={s.photos}
+            >
+                <Button
+                    href='https://disk.yandex.ru/d/nO97CIb7jZjImA'
+                    style={{
+                        position: 'absolute',
+                        top: 'calc(var(--section-spacing) * 1)',
+                        right: 'calc(var(--section-spacing) * 2)',
+                    }}
+                >
+                    всякие фотки
+                </Button>
+                <Image
+                    src={textImg}
+                    alt='текст на фоне людей'
+                />
+            </Section>
+        </>
+    )
+}
